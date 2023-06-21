3807ee001feda6cc653c381810bff3c6c736b82d
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-06-20 19:12:31 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - prtints alphabet in lowercase followed by  new line
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 0;
+while (i < 26)
+{
+_putchar(alphabet[i]);
+i++;
+}
+_putchar('\n');
+}
