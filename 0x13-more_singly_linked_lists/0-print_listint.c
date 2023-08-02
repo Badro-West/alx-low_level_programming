bb1dc8ad1cfb33662d97f0c62c05671ceea8c266
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-08-01 19:42:18 -0700

    kk

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,33 +1,31 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * main - check the code
- * 
- * Return: Always 0.
+ * print_listint_recursive - Prints a singly linked list of integers recursively.
+ * @h: Pointer to the head of the linked list.
+ * @nodes: Pointer to a variable to store the number of nodes.
+ * Return: Number of nodes in the linked list.
  */
-int main(void)
+void print_listint_recursive(const listint_t *h, size_t *nodes)
 {
-    listint_t *head;
-    listint_t *new;
-    listint_t hello = {8, NULL};
-    size_t n;
+    if (h == NULL)
+        return;
 
-    head = &hello;
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-    {
-        printf("Error\n");
-        return (1);
-    }
-    new->n = 9;
-    new->next = NULL; // Set the next pointer of the new node to NULL
-    hello.next = new; // Update the next pointer of the original head
-    n = print_listint(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (0);
+    (*nodes)++;
+    printf("%d\n", h->n);
+    print_listint_recursive(h->next, nodes);
+}
+
+/**
+ * print_listint - Prints a singly linked list of integers.
+ * @h: Pointer to the head of the linked list.
+ * Return: Number of nodes in the linked list.
+ */
+size_t print_listint(const listint_t *h)
+{
+    size_t nodes = 0;
+    print_listint_recursive(h, &nodes);
+    return nodes;
 }
 
