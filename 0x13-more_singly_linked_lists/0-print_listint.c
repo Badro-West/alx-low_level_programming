d432217555aaa49fbe60cce30c4617a9f7e05982
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-08-01 19:43:56 -0700

    kk

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,31 +1,17 @@
 #include "lists.h"
 #include <stdio.h>
 
-/**
- * print_listint_recursive - Prints a singly linked list of integers recursively.
- * @h: Pointer to the head of the linked list.
- * @nodes: Pointer to a variable to store the number of nodes.
- * Return: Number of nodes in the linked list.
- */
-void print_listint_recursive(const listint_t *h, size_t *nodes)
-{
-    if (h == NULL)
-        return;
 
-    (*nodes)++;
-    printf("%d\n", h->n);
-    print_listint_recursive(h->next, nodes);
-}
-
-/**
- * print_listint - Prints a singly linked list of integers.
- * @h: Pointer to the head of the linked list.
- * Return: Number of nodes in the linked list.
- */
 size_t print_listint(const listint_t *h)
 {
-    size_t nodes = 0;
-    print_listint_recursive(h, &nodes);
-    return nodes;
-}
+	size_t nodes = 0;
 
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
