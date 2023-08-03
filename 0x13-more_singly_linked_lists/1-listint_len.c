11c373bd67f51e7320fc8390c5a0479ab4febe7d
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-08-02 17:02:16 -0700

    ok

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,9 +8,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	if (h == NULL)
-		return 0;
+	size_t count = 0;
 
-	return 1 + listint_len(h->next);
+	do
+	{
+		count++;
+		h = h->next;
+	} while (h);
+
+	return count;
 }
 
