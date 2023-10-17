58304d75131a31e2c0aef6fa9e0b6961c2498704
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-10-16 17:04:13 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
-wget -p /tmp/win.so https://github.com/Badro-West/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.sh
+wget -o /tmp https://github.com/Badro-West/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.sh
 export LD_PRELOAD=/tmp/libgiga.so
