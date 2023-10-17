3f8b5c7bd3ccb9f9209b364a27bb499660cc76fc
Badro-West <badreddine.mezgueldi5@gmail.com>
2023-10-16 17:14:14 -0700

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,6 @@
 cp ./gm /tmp/gm
-export LD_PRELOAD=$(pwd)/libhack.so
-/tmp/gm 9 8 10 24 75 9
+
+echo -e '#include <stdio.h>\nunsigned int random() { return 9; }' > /tmp/hack.c
+gcc -shared -o /tmp/hack.so /tmp/hack.c
+LD_PRELOAD=/tmp/hack.so
+
